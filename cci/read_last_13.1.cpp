01699692dcbc2a2cf85e168e7af9593cfbbeace5
ksadhu <kumarsnaga@gmail.com>
2013-12-23 22:03:06 -0500

    working code

--- cci/read_last_13.1.cpp
+++ cci/read_last_13.1.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int main(){
 	ifstream fin("test.txt");
 	ifstream fin1("test.txt");
-	string s1,s2,lines[10];
+	string s1,s2;
 	int count=0;
 	if(fin.is_open() and fin1.is_open()){
 		int i=0;
 		while(i<10){
-			while(getline(fin, s1)){
-			}
+			getline(fin, s1);
 			i++;
 		}
 		while(getline(fin,s1))
